d30b866403ebf872af94a7b949059f41a9c7d8b9
Samuel Kinuthia Hinga <shinga@safaricom.co.ke>
2023-03-12 23:03:10 +0300

    Nested state

---
+++ nested-state.js
@@ -0,0 +1,48 @@
+const redux = require('redux')
+const createStore = redux.createStore
+
+const CHANGE_STREET = 'CHANGE_STREET'
+
+// action
+
+function changeStreet (str) {
+    return {
+        type: CHANGE_STREET,
+        payload: str
+    }
+}
+
+const initialState = {
+    name: 'Hinga',
+    address: {
+        street: 'Koinange Street',
+        city: 'Nairobi',
+        state: 'Blanco',
+    },
+    age: 'Young',
+}
+
+//reducer
+
+const reducer = (state = initialState, action) => {
+    switch (action.type){
+        case CHANGE_STREET:
+            return {
+                ...state,
+                address: {
+                    ...state.address,
+                    street: action.payload,
+                }
+            }
+        default: state
+    }
+}
+
+const store = createStore(reducer)
+// console.log('initial state', store.getState())
+
+const unsubscribe = store.subscribe(()=> console.log('Update state ', store.getState()))
+
+store.dispatch (changeStreet('Kitusulu'))
+
+unsubscribe()
