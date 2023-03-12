d0c3116b142e5430d5c77c3bda7fe472c0972683
Samuel Kinuthia Hinga <shinga@safaricom.co.ke>
2023-03-12 22:28:19 +0300

    Multiple reducers combine

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const redux = require('redux')
 const createStore = redux.createStore
-
 const bindActionCreators = redux.bindActionCreators
+const combinedReducers = redux.combineReducers
 
 const DRINK_ORDERED = 'DRINK_ORDERED'
 const DRINK_RESTOCKED = 'DRINK_RESTOCKED'
@@ -41,14 +41,22 @@ function restockChaser (qty=1){
 
 // (previousState) as an object
 
-const initialState = {
+// const initialState = {
+//     numOfDrinks: 10,
+//     numofChasers: 20,
+// }
+
+const initialDrinksState = {
     numOfDrinks: 10,
+}
+
+const initialChasersState = {
     numofChasers: 20,
 }
 
 // (previousState, action) => newState (reducer)
 
-const reducer = (state = initialState, action) => {
+const drinksReducer = (state = initialDrinksState, action) => {
     switch(action.type){
         case DRINK_ORDERED:
             return {
@@ -60,17 +68,34 @@ const reducer = (state = initialState, action) => {
                 ...state,
                 numOfDrinks: state.numOfDrinks + action.payload,
             }
+        default:
+            return state
+    }
+}
+
+const chasersReducer = (state = initialChasersState, action) => {
+    switch(action.type){
         case CHASER_ORDER:
             return {
                 ...state,
                 numofChasers: state.numofChasers - 1,
+            }
+        case CHASER_RESTOCK:
+            return {
+                ...state,
+                numofChasers: state.numofChasers + action.payload,
             }
         default:
             return state
     }
 }
 
-const store = createStore(reducer)
+const rootReducer = combinedReducers({
+    drink: drinksReducer,
+    chaser: chasersReducer
+})
+
+const store = createStore(rootReducer)
 console.log('initial state', store.getState())
 
 const unsubscribe = store.subscribe(() => console.log('Update state ', store.getState()))
@@ -80,12 +105,13 @@ const unsubscribe = store.subscribe(() => console.log('Update state ', store.get
 // store.dispatch ( orderDrink())
 // store.dispatch ( restockDrink(3))
 
-const actions = bindActionCreators({orderDrink, restockDrink, orderChaser}, store.dispatch)
+const actions = bindActionCreators({orderDrink, restockDrink, orderChaser, restockChaser}, store.dispatch)
 actions.orderDrink()
 actions.orderDrink()
 actions.orderDrink()
 actions.orderChaser()
 actions.restockDrink(3)
+actions.restockChaser(2)
 unsubscribe()
 
 // console.log('Latter state', store.getState())
