3a45ed54c961e2f8092264a9661e4523c5920343
Samuel Kinuthia Hinga <shinga@safaricom.co.ke>
2023-03-12 15:45:32 +0300

    Multiple states in single reducer

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const bindActionCreators = redux.bindActionCreators
 const DRINK_ORDERED = 'DRINK_ORDERED'
 const DRINK_RESTOCKED = 'DRINK_RESTOCKED'
 
+const CHASER_ORDER = 'CHASER_ORDER'
+const CHASER_RESTOCK = 'CHASER_RESTOCK'
+
 // (action)
 
 function orderDrink(){
@@ -22,10 +25,25 @@ function restockDrink(qty=1){
     }
 }
 
+function orderChaser (){
+    return {
+        type: CHASER_ORDER,
+        payload: 1,
+    }
+}
+
+function restockChaser (qty=1){
+    return {
+        type: CHASER_RESTOCK,
+        payload: qty,
+    }
+}
+
 // (previousState) as an object
 
 const initialState = {
     numOfDrinks: 10,
+    numofChasers: 20,
 }
 
 // (previousState, action) => newState (reducer)
@@ -42,6 +60,11 @@ const reducer = (state = initialState, action) => {
                 ...state,
                 numOfDrinks: state.numOfDrinks + action.payload,
             }
+        case CHASER_ORDER:
+            return {
+                ...state,
+                numofChasers: state.numofChasers - 1,
+            }
         default:
             return state
     }
@@ -57,10 +80,11 @@ const unsubscribe = store.subscribe(() => console.log('Update state ', store.get
 // store.dispatch ( orderDrink())
 // store.dispatch ( restockDrink(3))
 
-const actions = bindActionCreators({orderDrink, restockDrink}, store.dispatch)
+const actions = bindActionCreators({orderDrink, restockDrink, orderChaser}, store.dispatch)
 actions.orderDrink()
 actions.orderDrink()
 actions.orderDrink()
+actions.orderChaser()
 actions.restockDrink(3)
 unsubscribe()
 
